fd56552971f5fee8019c3e6ca48a556b8a045228
BazanJC <kori1ryu@gmail.com>
2021-03-30 08:57:55 -0400

    base de interfaz

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include "templates/header.php" ?>
+    <h1>Universida Mayor de San Andres</h1>
+    <h3>Bazan Panozo Juan Carlos</h3>
+<?php include "templates/footer.php"; ?>
---
+++ templates/footer.php
@@ -0,0 +1,4 @@
+<h5>La Paz - Bolivia </h5>
+<h6>2021</h6>
+</body>
+</html>
